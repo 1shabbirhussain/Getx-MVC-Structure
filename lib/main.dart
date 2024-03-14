1269569f6f65fe25ae6928179bff903d4e1299f0
1shabbirhussain <001.shabbirhussain@gmail.com>
2024-03-14 10:40:54 +0500

    comments added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:getx_mvc/presentation/product_modules/views/internationalization
 import 'package:getx_mvc/resources/common_modules/internationalization/internationalization_languages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'core/bindings/bindings.dart';
 
 void main() {
@@ -12,17 +11,16 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "INternationalization",
-      locale: Locale("en", "US"),
-      fallbackLocale: Locale("en","US"),
+      title: "Internationalization",
+      locale: const Locale("en", "US"), // default lcoale
+      fallbackLocale: const Locale("en","US"), //if locale is not available fall back to english
       translations: MyTranslations(),
-       initialBinding: InitialBinding(),
+      //  initialBinding: InitialBinding(),
       debugShowCheckedModeBanner: false,
-      home: LocalizationInternationalization(),
+      home: const LocalizationInternationalization(),
     );
   }
 }
